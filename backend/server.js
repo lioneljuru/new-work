053a39bc97263039f64e4586ad3fe318389833e1
lioneljuru <lioneljuru@gmail.com>
2025-05-23 21:10:53 +0200

    moving database to atlas

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,10 @@ app.set("trust proxy", true);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
