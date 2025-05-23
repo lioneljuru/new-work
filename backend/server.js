61d332062a534d98c6481874fd3fed41719407a9
lioneljuru <lioneljuru@gmail.com>
2025-05-23 23:54:59 +0200

    fixed some error in server.js

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ app.set("trust proxy", true);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true,
 })
 .then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
